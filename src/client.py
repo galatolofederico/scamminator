4338c5d574feecf75316f0ede8c9864c7c655b8a
Federico Galatolo <galatolo.federico@gmail.com>
2021-08-08 22:25:26 +0200

    Client RPC interface

--- src/client.py
+++ src/client.py
@@ -1,9 +1,14 @@
+import os
+import logging
 import time
 import random
+import pickle
 from pytg.receiver import Receiver
 from pytg.sender import Sender
 from pytg.utils import coroutine
 
+import threading
+from xmlrpc.server import SimpleXMLRPCServer
 
 class Client:
     def __init__(self):
@@ -64,11 +69,66 @@ class Client:
             cache_file.close()
 
     def start(self):
+        logging.info("Starting telegram client")
         self.receiver.start()
         self.receiver.message(self.reply(self.sender))
 
         self.receiver.stop()
 
-if __name__ == "__main__":
-    client = Client()
+    def dialog_list(self):
+        return self.sender.dialog_list()
+    
+    def add(self, peer_id, name):
+        self.active_users[peer_id] = name
+        self.save_cache()
+        return True
+    
+    def remove(self, name):
+        if name not in self.active_users:
+            return False
+        else:
+            del self.active_users[name]
+            self.save_cache()
+            return True
+
+    def list(self):
+        return self.active_users
+
+class RPCWrapper:
+    def __init__(self, client):
+        self.client = client
+
+    def search(self, name):
+        ret = []
+        for user in self.client.dialog_list():
+            if name in user["print_name"]:
+                ret.append(dict(name=user["print_name"], peer_id=user["peer_id"]))
+        return ret
+    
+    def add(self, peer_id, name):
+        return self.client.add(peer_id, name)
+    
+    def remove(self, peer_id):
+        return self.client.remove(peer_id)
+    
+    def list(self):
+        return self.client.list()
+
+def start_client(client):
+    logging.info("Starting client")
     client.start()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    client = Client()
+    server = SimpleXMLRPCServer((os.environ["RPC_HOST"], int(os.environ["RPC_PORT"])))
+
+    threading.Thread(target=start_client, args=(client, )).start()
+
+    wrapper = RPCWrapper(client)
+    server.register_instance(wrapper)
+    
+    logging.info("Starting RPC Server")
+    server.serve_forever()
+    
+    
